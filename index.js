062370642b101e55a669b2f962eccf9994543c90
LAPTOP-6A2IC92J\rdhd1 <junova.kristyna@gmail.com>
2023-03-12 14:57:01 +0100

    du2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('funguju!');
+
+const kostka = document.querySelector('.dice')
+let side = 1
+const otoceni = () => {
+    side = side + 1
+    if (side === 7){
+        side = 1
+    }
+    kostka.src = `img/side${side}.svg`
+}
+
+document.addEventListener('keydown', otoceni)
